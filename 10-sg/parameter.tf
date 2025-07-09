0007498a6cdc6171126c688911fe3fb6ddfb9c4a
vinodh241 <a.vinodh9072@gmail.com>
2025-07-09 11:33:19 +0530

    roboshop-infra-dev

--- 10-sg/parameter.tf
+++ 10-sg/parameter.tf
@@ -2,28 +2,33 @@ resource "aws_ssm_parameter" "frontend_sg_id" {
   name = "/${var.project}/${var.environment}/frontend_sg_id"
   type  = "String"
   value = module.frontend.sg_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project}/${var.environment}/bastion_sg_id"
   type  = "String"
   value = module.bastion.sg_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "backend_alb_sg_id" {
   name = "/${var.project}/${var.environment}/backend_alb_sg_id"
   type  = "String"
   value = module.backend_alb.sg_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "vpn_sg_id" {
   name = "/${var.project}/${var.environment}/vpn_sg_id"
   type  = "String"
   value = module.vpn.sg_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project}/${var.environment}/mongodb_sg_id"
   type  = "String"
   value = module.mongodb.sg_id
+  overwrite = true
 }
