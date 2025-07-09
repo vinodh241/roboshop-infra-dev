d135b71f6e78703146aea359ba3e04b1bc774b1e
vinodh241 <a.vinodh9072@gmail.com>
2025-07-09 16:22:03 +0530

    roboshop-infra-dev

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -3,4 +3,4 @@ component=$1
 
 dbf install ansible -y 
 
-ansible-pull -U https://github.com/daws-84s/ansible-roboshop-roles-tf.git -e component=$1 main.yaml
+ansible-pull -U https://github.com/vinodh241/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
